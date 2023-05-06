0cae58af8b778cbbe04b96baf852dadc3ba6e780
amie15 <109030666+amie15@users.noreply.github.com>
2023-05-06 23:45:36 +0900

    ><

--- 1-13/src/test1.java
+++ 1-13/src/test1.java
@@ -1,3 +1,80 @@
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//public class test1 {
+//	public static void main(String[] args) {
+//		//改行
+//		System.out.println("問１");
+//		//問題1:下記の条件を使ってfor文と条件文を組み合わせ結果が同じになるよう出力しなさい。for文ネスト3回（3回forを書くこと）
+//		//		例）
+//		//		for() {
+//		//			for() {
+//		//				for() {
+//		//					
+//		//				}
+//		//			}
+//		//		}
+//		//strArray1でfor文を回し3文字目(k)以降のものをstrArray2に格納する(1個目のfor文)
+//		//strArray2でfor文を回しkとaとoの物だけでstrArray3に格納する(2個目のfor文)
+//		//strArray3でfor文を回し「kakao」となるようにstrArray4に格納しstrArray4を出力する。(3個目のfor文)
+//		List<String> strArray1 = new ArrayList(Arrays.asList("f", "u", "k", "u", "d", "a", "k" , "a" , "n" , "e" , "k" , "o" ));
+//		List<String> strArray2 = new ArrayList<String>();
+//		List<String> strArray3 = new ArrayList<String>();
+//		List<String> strArray4 = new ArrayList<String>();
+//
+//		for(int i = 2; i < strArray1.size(); i++) {
+//
+//			strArray2.add(strArray1.get(i));
+//
+////			if(i == (strArray1.size() - 1)) {
+//
+//				for(int a = i-2; a < strArray2.size(); a++) {
+//					//			for(String str2 : strArray2) {
+//
+//					if(strArray2.get(a) == "k" || strArray2.get(a) == "a" || strArray2.get(a) == "o") {
+//						strArray3.add(strArray2.get(a));
+//					
+////						if(a == (strArray2.size() - 1)) {
+//
+//							for(int b = 0; b < strArray3.size(); b++) {
+//
+//								if( b == 0 || b == 4 || b ==5 || b == 6 || b == 11) {
+//									strArray4.add(strArray3.get(b));
+//								}
+////								else if(strArray3.get(b) == "a" && b == 1) {
+////									strArray4.add(strArray3.get(b));
+////								}
+////
+////								else if(strArray3.get(b) == "k" && b == 2) {
+////									strArray4.add(strArray3.get(b));
+////								}
+////
+////								else if(strArray3.get(b) == "a" && b == 3) {
+////									strArray4.add(strArray3.get(b));
+////								}
+////
+////								else if(strArray3.get(b) == "o" && b == 5){
+////									strArray4.add(strArray3.get(b));
+////								}
+//							
+//
+////						}
+//					}
+//
+//				}
+//			}
+//		}
+//
+//
+//		System.out.println(strArray4.toString());
+//
+//	}
+//}
+//
+//
+
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -70,5 +147,4 @@ public class test1 {
 		
 	}
 }
-
 
--- 1-15/src/test1.java
+++ 1-15/src/test1.java
@@ -1,28 +1,77 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class test1 {
 	public static void main(String[] args) {
 		//改行
 		System.out.println("問１");
 		//問１のメソッドを呼び出して出力してください（引数５と１０）
+		
+		q1 (5,10);
 		//改行
 		System.out.println("問２");
 		//問２のメソッドを呼び出して出力してください（引数"しんぶんし"）
+		q2 ("しんぶんし");
 		//改行
 		System.out.println("問３");
 		//問3のメソッドを呼び出して出力してください（引数,配列[5,3,1,9,4,8]）
 		Integer [] i = {5,3,1,9,4,8};
+		Toi3.q3(i);
 		//改行
 		System.out.println("問４");
 		//問4のメソッドを呼び出して出力してください（引数,配列[5,3,1,9,4,8]）
 		int [] k = {5,3,1,9,4,8};
+		Toi4.q4(k);
 	}
 	//問題1:2つの整数を受け取り、それらの和を返すメソッドを作成してください。
 	
+	public static void q1(int num1 ,int num2) {
+		System.out.println(num1 + "," + num2 );
+	}
+	
 	//問題2:文字列を受け取り、その文字列が回文であるかどうかを返すメソッドを作成してください。
 	
+	public static void q2(String kaibun) {
+		
+		String result = new StringBuilder(kaibun).reverse().toString();
+		
+		if (result == kaibun) {
+			System.out.println("この文字列は回文です");
+		}
+		
+		else {
+			System.out.println("この文字列は回文ではありません");
+		}
+		
+	}
+	
+	
 	//問題3:整数の配列を受け取り、その配列の要素を降順にソートするメソッドを作成してください。(for文は利用しないこと)
+
+	class Toi3{
+		static void q3(Integer[] a) {
+			Arrays.sort(a, Comparator.reverseOrder());
+			System.out.println( Arrays.toString(a));
+		}
+	}
 	
 	//問題4:整数の配列を受け取り、その配列の中で最小値を持つ要素のインデックスを返すメソッドを作成してください。
+	class Toi4{
+		static void q4(int[] b) {
+			int n = 0;
+			for(int i =0; i<b.length; i++) {
+				if(i==0) {
+					n = b[i];
+				}
+				else if(b[i]<n) {
+					n = b[i];
+				}
+				
+			}
+			System.out.println(n);
+		}
 	
+	}
 	
 }
 
