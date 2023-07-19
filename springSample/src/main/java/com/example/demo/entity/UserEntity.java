43b505038c1bae407a747da1a57a3af83f36bc03
amie15 <109030666+amie15@users.noreply.github.com>
2023-07-19 21:50:32 +0900

    3-12

--- springSample/src/main/java/com/example/demo/entity/UserEntity.java
+++ springSample/src/main/java/com/example/demo/entity/UserEntity.java
@@ -3,6 +3,14 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 
 /**
 * ユーザー情報 Entity
